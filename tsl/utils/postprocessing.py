99f815a05847f34c00eb0771beb9574b4fb5dc3b
GS <g.simon@of2m.fr>
2022-06-14 14:54:43 +0200

    add a function for recustruct the dataframe from batchs

--- tsl/utils/postprocessing.py
+++ tsl/utils/postprocessing.py
@@ -2,16 +2,27 @@ from pdb import set_trace as bp
 import numpy as np
 import pandas as pd
 
-def tensor_to_df(y, ind, cols):
-    n_batchs, n_times, n_nodes, n_channels = y.shape
+def array_to_df(
+        y: np.ndarray,
+        ind: pd.core.indexes.datetimes.DatetimeIndex,
+        cols: pd.core.indexes.multi.MultiIndex
+) -> pd.DataFrame:
+    '''
+    Recconstruct time series using MultiIndex columns dataframe.
+    '''
+    n_batchs, window, n_nodes, n_channels = y.shape
     r = len(ind)-n_batchs
-    
-    bp()
-    np.vstack((y[:,0,:, :], y[-1,r ,:, :]))
-
-    return pd.DataFrame(
+    Y = np.vstack(
+        (
+            y[:,0,:, :],
+            y[-1,r ,:, :].reshape(r, n_nodes, n_channels)
+        )
+    )
+    n_times = Y.shape[0]
+    df = pd.DataFrame(
         index=ind,
-        data=y.reshape(n_batchs*n_times, n_nodes*n_channels),
+        data=Y.reshape(n_times, n_nodes*n_channels),
         columns=cols
     )
+    return df
 
