d95c586345c6b6ebcf8a5f07684175bfdcacb812
GS <g.simon@of2m.fr>
2022-06-14 17:30:09 +0200

    save_ts function

--- tsl/utils/postprocessing.py
+++ tsl/utils/postprocessing.py
@@ -2,6 +2,10 @@ from pdb import set_trace as bp
 import numpy as np
 import pandas as pd
 
+from tsl.data.utils import SynchMode
+from tsl.ops.imputation import prediction_dataframe
+from tsl.data.datamodule.spatiotemporal_datamodule import SpatioTemporalDataModule
+
 def array_to_df(
         y: np.ndarray,
         ind: pd.core.indexes.datetimes.DatetimeIndex,
@@ -26,3 +30,29 @@ def array_to_df(
     )
     return df
 
+
+def save_ts(
+        tag: str,
+        path_results: str,
+        y_hat: np.ndarray,
+        y_true: np.ndarray,
+        dm: SpatioTemporalDataModule,
+        dataset,
+):
+    '''
+    Convert batch prediction to times series and save it in HDF5 store.
+    '''
+    df_hat = prediction_dataframe(
+        y_hat,
+        dm.torch_dataset.data_timestamps(dm.testset.indices)[SynchMode.HORIZON],
+        dataset.df.columns,
+        aggregate_by='mean'
+    )
+    df_true = prediction_dataframe(
+        y_true,
+        dm.torch_dataset.data_timestamps(dm.testset.indices)[SynchMode.HORIZON],
+        dataset.df.columns,
+        aggregate_by='mean'
+    )
+    df_hat.to_hdf(path_results, key=f"hat_{tag}")
+    df_true.to_hdf(path_results, key=f"true_{tag}")
